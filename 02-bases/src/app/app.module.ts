efceabb8773c38575e73d478b2b687fcba814971
GreizelSension <greizelsension@gmail.com>
2025-01-14 21:46:47 -0500

    subiendo tarea de module heroe y uso de common module

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,23 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import{HeroModule} from './heroes/heroes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
-],
+    CounterModule,
+    HeroModule,
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
